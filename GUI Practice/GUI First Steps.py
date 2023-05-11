dc81b974483e234e9e9caade501b9ece7f990033
Bistion <80474966+Bistion@users.noreply.github.com>
2023-05-11 11:40:10 -0400

    Initial Push

---
+++ GUI Practice/GUI First Steps.py
@@ -0,0 +1,15 @@
+# Need to import all of the tkinter module
+from tkinter import *
+from turtle import back
+
+# Create the actual GUI as a variable
+gui = Tk()
+
+# Set a background color
+gui.configure(background="white")
+
+# Set GUI size
+gui.geometry("500x500")
+
+# Launch the GUI window
+gui.mainloop()
